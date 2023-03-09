f8c253b01ac4b380e26f8d1238dbb11fe71bb40e
vishnudhaked <120500117+vishnudhaked@users.noreply.github.com>
2023-03-09 10:23:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os 
 
-if os.path.exists("/home/ubuntu/file2.txt")==True:
+if os.path.exists("/home/ec2-user/file2.txt")==True:
     print("file exists here")
 else:
     print("file does not exists")
